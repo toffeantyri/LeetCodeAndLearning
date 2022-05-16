c3c45efff86d5cebda2850198bd0b589b949b8ea
toffeantyri <tofy3434@yandex.ru>
2022-05-16 16:50:09 +0600

    1,3,4 задания Accessed

--- src/main/kotlin/leetcode/problems/algorithm/example 3.kt
+++ src/main/kotlin/leetcode/problems/algorithm/example 3.kt
@@ -1,29 +1,70 @@
 package leetcode.problems.algorithm
 
+import java.util.*
 
+
+//5% лучше чем другие
 class Solution3 {
     fun lengthOfLongestSubstring(s: String): Int {
         val array = arrayListOf<HashSet<Char>>(hashSetOf())
-        for ((i,v) in s.withIndex()) {
+        for ((i, v) in s.withIndex()) {
             loop@ for (char in s.subSequence(i, s.length)) {
                 if (array[i].contains(char)) {
-                    array.add(hashSetOf())
+
                     break@loop
                 } else array[i].add(char)
             }
             array.add(hashSetOf())
         }
-        for (i in array) {
-            println(i.toString())
-        }
-        return array.maxOf { it.size }
+        return array.map { it.size }.max() ?: 0
     }
+
+    //83% не могу понять...
+    fun lengthOfLongestSubstringE(s: String): Int {
+        val map: MutableMap<Char, Int> = HashMap()
+        var start = 0
+        var len = 0
+        for (i in s.indices) {
+            val c: Char = s[i]
+            if (map.containsKey(c)) {
+                if (map[c]!! >= start) start = map[c]!! + 1
+            }
+            len = Math.max(len, i - start + 1)
+            map[c] = i
+        }
+
+        return len
+    }
+
+        //77% счёт с двух сторон идёт
+    fun lengthOfLongestSubstringET(s: String): Int {
+        var count = 1
+        var j_start = 0
+        var max_count = 1
+            if (s.isEmpty()) {
+            return 0
+        } else if (s.length==1) return 1
+
+        var i = 1
+        while (i<s.length) {
+            for (j in j_start until i) {
+                if (s[i] != s[j]) count++ else j_start = j + 1
+            }
+            if (max_count < count) max_count = count
+            count = 1
+            i++
+        }
+        return max_count
+    }
+
+
 }
 
-//wke
+
 fun main() {
     val clz = Solution3()
-    println(clz.lengthOfLongestSubstring("dvdf"))
-
+    println(clz.lengthOfLongestSubstring("au"))
+    println(clz.lengthOfLongestSubstringE("au"))
+    println(clz.lengthOfLongestSubstringET("au"))
 
 }
